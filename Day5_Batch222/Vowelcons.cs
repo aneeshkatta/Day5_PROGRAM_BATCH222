272b74b211921e911d6e8a606fd18193c676610f
Aneesh <katta.aneesh597@gmail.com>
2022-11-15 23:20:08 +0530

    added Q9_VowelConsonentcheck

--- Day5_Batch222/Vowelcons.cs
+++ Day5_Batch222/Vowelcons.cs
@@ -10,7 +10,29 @@ namespace Day5_Batch222
     {
         public void vowelcons()
         {
-
+            Console.WriteLine("Enter an Alphabet(A-Z or a-z) to check its a Vowel or Consonent");
+            char input = Convert.ToChar(Console.ReadLine().ToLower());
+            switch (input)
+            {
+                case 'a':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                default:
+                    Console.WriteLine("The Alphabet is Consonent");
+                    break;
+            }
         }
     }
 }
