b3d93541267774badac300e60a6b5c6f3579d1bc
Aneesh <katta.aneesh597@gmail.com>
2022-11-15 23:04:28 +0530

    added Q7_swapnumber

--- Day5_Batch222/Swapnum.cs
+++ Day5_Batch222/Swapnum.cs
@@ -10,6 +10,20 @@ namespace Day5_Batch222
     {
         public void swapnum()
         {
+            Console.WriteLine("Enter a number A:");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number B");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before swapping numbers");
+            Console.WriteLine("A=" + A);
+            Console.WriteLine("B=" + B);
+            int temp;
+            temp = A;
+            A = B;
+            B = temp;
+            Console.WriteLine("After swapping numbers");
+            Console.WriteLine("A=" + A);
+            Console.WriteLine("B=" + B);
 
         }
     }
