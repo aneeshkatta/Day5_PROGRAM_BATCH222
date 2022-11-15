b0aa30b4ecf77242d25860816d807c94e42af52d
Aneesh <katta.aneesh597@gmail.com>
2022-11-15 21:00:40 +0530

    added harmonic number solution

--- Day5_Batch222/HarmonicNo.cs
+++ Day5_Batch222/HarmonicNo.cs
@@ -10,6 +10,15 @@ namespace Day5_Batch222
     {
         public void harmonicno()
         {
+            Console.WriteLine("Enter a  number N to get harmonic series sum in the series 1/1 + 1/2 + ... + 1/N ");
+            int num= Convert.ToInt32(Console.ReadLine()); 
+            double b = 0.00;
+            for(double i=1;i<=num;i++)
+            {
+                Console.WriteLine("1/{0}+", i);
+                b = 1 + (1 / i);
+            }
+            Console.WriteLine("harmonic series sum is :" + b);
 
         }
     }
