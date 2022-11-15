0e9ccd72ff6ddf5b8d436dc79a55b4a59e1c9849
Aneesh <katta.aneesh597@gmail.com>
2022-11-15 22:50:44 +0530

    added Q6_QuotientRemaindr

--- Day5_Batch222/QuotientRem.cs
+++ Day5_Batch222/QuotientRem.cs
@@ -10,7 +10,14 @@ namespace Day5_Batch222
     {
         public void quotientrem()
         {
-
+            Console.WriteLine("Enter dividend value to get quotient and remainder");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter divisor value to get quotient and remainder");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int quotient = Convert.ToInt32((num / num1));
+            int remainder = (num % num1);
+            Console.WriteLine("Quotient is {0}", quotient);
+            Console.WriteLine("Remainder is {0}", remainder);
         }
     }
 }
