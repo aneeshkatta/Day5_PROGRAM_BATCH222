cc979bf2d930e1eb392461fcdc8501a0db5463bb
Aneesh <katta.aneesh597@gmail.com>
2022-11-15 23:09:56 +0530

    added evenodd program

--- Day5_Batch222/Evenodd.cs
+++ Day5_Batch222/Evenodd.cs
@@ -10,7 +10,16 @@ namespace Day5_Batch222
     {
         public void evenodd()
         {
-
+            Console.WriteLine("Enter a number to check its even or odd");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("no. is a even");
+            }
+            else
+            {
+                Console.WriteLine("no. is a Odd");
+            }
         }
     }
 }
