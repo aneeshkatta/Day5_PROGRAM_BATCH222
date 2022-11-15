52b53e6def50a495b6a043cd31a0711046b09e0b
Aneesh <katta.aneesh597@gmail.com>
2022-11-15 20:26:33 +0530

    added main

--- Day5_Batch222/Flipcoin.cs
+++ Day5_Batch222/Flipcoin.cs
@@ -10,27 +10,7 @@ namespace Day5_Batch222
     {
         public void flipcoin()
         {
-            Console.WriteLine("Enter number of times to toss ");
-            double num = Convert.ToDouble(Console.ReadLine());
-            Random random = new Random();
-            double headcount = 0.0;
-            double tailcount = 0.0;
-            for(int i=1;i<num;i++)
-            {
-                double coinflipresult = random.NextDouble();
-                if(coinflipresult<0.5)
-                {
-                    Console.WriteLine("Tails won the game");
-                    tailcount++;
-                }
-                else
-                {
-                    Console.WriteLine("Heads won the game");
-                    headcount++;
-                }
-            }
-            Console.WriteLine("Heads won by {0}%", (headcount / num));
-            Console.WriteLine("Tails won by {0}%", (tailcount / num));
+           
         }
     }
 }
