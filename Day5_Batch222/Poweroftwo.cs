08e35336710748cee5ba97034216c79125792e4a
Aneesh <katta.aneesh597@gmail.com>
2022-11-15 20:47:42 +0530

    added Q3_poweroftwo solution

--- Day5_Batch222/Poweroftwo.cs
+++ Day5_Batch222/Poweroftwo.cs
@@ -10,7 +10,12 @@ namespace Day5_Batch222
     {
         public void poweroftwo()
         {
-
+            Console.WriteLine("Enter a number to get table of 2^N");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i=1;i<=num;i++)
+            {
+                Console.WriteLine("{0}*{1}={2}", 2, Math.Pow(2, i - 1), 2 * Math.Pow(2, i - 1));
+            }
         }
     }
 }
