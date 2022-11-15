d84861060502667ef89776dce229d370a5e8ee65
Aneesh <katta.aneesh597@gmail.com>
2022-11-15 21:13:49 +0530

    added Q5_Primefactors solution

--- Day5_Batch222/PrimeFactor.cs
+++ Day5_Batch222/PrimeFactor.cs
@@ -10,7 +10,15 @@ namespace Day5_Batch222
     {
         public void primefactor()
         {
-
+            Console.WriteLine("Enter a number to get prime factors with Logic -> Traverse till i*i <= N instead of i <= N");
+            int N = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i*i<=N;i++)
+            {
+                if (N % i ==0)
+                {
+                    Console.WriteLine("{0} is a factor of {1}", i, N);
+                }
+            }
         }
     }
 }
