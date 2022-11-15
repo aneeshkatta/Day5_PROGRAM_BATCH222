bbb3bd1a73a1473e3a97089db165b201a7710793
Aneesh <katta.aneesh597@gmail.com>
2022-11-15 23:27:34 +0530

    added Q10_LargestofThreeNos

--- Day5_Batch222/Largest3Nos.cs
+++ Day5_Batch222/Largest3Nos.cs
@@ -10,7 +10,24 @@ namespace Day5_Batch222
     {
         public void largest3Nos()
         {
-
+            Console.WriteLine("Enter a number1 to get largest of three numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number2 to get largest of three numbers");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number3 to get largest of three numbers");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2 && num2 > num3)
+            {
+                Console.WriteLine("num1 is Largest Number");
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("num2 is Largest Number");
+            }
+            else
+            {
+                Console.WriteLine("num3 is Largest Number");
+            }
         }
     }
 }
